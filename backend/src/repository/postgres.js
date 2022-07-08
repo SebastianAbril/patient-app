fd50d98f3dd2d9c2cd5449e81d25e49e7b315b90
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-07-08 15:43:31 -0500

    added ssl configuration to postgres.js

--- backend/src/repository/postgres.js
+++ backend/src/repository/postgres.js
@@ -4,7 +4,10 @@ const pool = new Pool({
   port: parseInt(process.env.DATABASE_PORT),
   database: process.env.DATABASE_NAME,
   user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD
+  password: process.env.DATABASE_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pool };
